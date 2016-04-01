9b921df72017c78d4f0804ca6c843ed559e7344c
Joseph Tompkins <joseph.tompkins@gmail.com>
2016-04-01 17:03:41 -0500

    added ActivityEventListView and associated layout

---
+++ app/src/main/java/com/example/oose/sublimecalendar/ActivityEventListView.java
@@ -0,0 +1,24 @@
+package com.example.oose.sublimecalendar;
+
+import android.app.Activity;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+public class ActivityEventListView extends AppCompatActivity {
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
